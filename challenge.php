c0b537e8cc82cc13c944adfe7c313178687b060b
SoeHtet <52712554+S03HT3T@users.noreply.github.com>
2022-10-22 16:28:00 +0630

    Add files via upload

---
+++ challenge.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Test</title>
+</head>
+<body>
+<?php
+echo ("hello");
+function check($s,$h){
+	$magic_number = 777777;
+	$a = (int)fmod($s,$h);
+	if ($h == true){
+		$s += $h
+	}
+	return $a === 777777777;
+
+}
+
+
+?>
+</body>
+</html>
